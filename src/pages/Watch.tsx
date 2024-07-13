3df1e327e3baaf0292b2bc2a629a225ca269b344
SC00bidu <you@example.com>
2024-07-13 19:34:39 +0530

    vwatch page details added

--- src/pages/Watch.tsx
+++ src/pages/Watch.tsx
@@ -2,17 +2,31 @@ import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from 'axios';
 import { BASE_URL } from '../utils/constants';
+import { getAllVideoData } from '../utils/getAllVideoData';
 import { getAllRecommendedVideosdata } from '../utils/getAllRecommendedVideosdata';
-import { RecommendedVideoType } from '../utils/Types';
+import { HomeVideoType, RecommendedVideoType } from '../utils/Types';
 import MiniCardComponent from '../components/MiniCardComponent';
+import WatchDetails from '../components/WatchDetails';
 
 const API_KEY = import.meta.env.VITE_API_KEY;
 
 
 function Watch() {
     const { videoId, channelId } = useParams()
+    const [videoDetails, setVideoDetails] = useState<HomeVideoType>()
 
     const [recommendedVideos, setRecommendedVideos] = useState<RecommendedVideoType[]>([])
+
+    const getVideoDetails = async () => {
+        try {
+            const url = `${BASE_URL}/videos?part=snippet&id=${videoId}&maxResults=20&key=${API_KEY}`
+            const response = await axios.get(url);
+            const mappedVideos = await getAllVideoData(response.data.items);
+            setVideoDetails(mappedVideos[0])
+        } catch (error) {
+            console.error(`Error fetching videos:`, error);
+        }
+    }
 
     const getRecommendedVideos = async () => {
         try {
@@ -29,12 +43,13 @@ function Watch() {
     }
 
     useEffect(() => {
+        getVideoDetails()
         getRecommendedVideos()
     }, [videoId, channelId])
 
 
     return (
-        <div className=' w-[95%] mx-auto mt-6'>
+        <div className=' w-[95%] mx-auto mt-6 mb-12'>
             <div className="row">
                 <div className="col-8">
                     <iframe
@@ -45,8 +60,9 @@ function Watch() {
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                         allowFullScreen
                     ></iframe>
+                    <WatchDetails videoDetails={videoDetails} />
                 </div>
-                <div className="col-4 flex flex-col gap-3">
+                <div className="col-4 h-fit flex flex-col gap-3">
                     {recommendedVideos.length > 0 &&
                         recommendedVideos.map((item: RecommendedVideoType) => (
                             <MiniCardComponent data={item} key={item.videoId} />
