557dc607cc1e6251c89814e60bd1742af7140d57
SC00bidu <you@example.com>
2024-07-18 01:49:45 +0530

    made sidebar responsive

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -89,14 +89,14 @@ export default function Sidebar({ filter, setFilter, setCategoryId }:
 
   return (
     <div data-bs-dismiss="offcanvas" className="w-full h-full text-white bg-[#0c0c0c] overflow-auto pb-8 sidebar">
-      <div className="flex gap-8 items-center text-2xl w-[80%] mx-auto h-14">
+      <div className="flex sm:gap-8 gap-3 items-center text-2xl w-[85%] mx-auto sm:h-14 h-12">
         <a className="" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
-          <RxHamburgerMenu className='text-xl' />
+          <RxHamburgerMenu className='sm:text-xl text-lg' />
         </a>
         <Link to="/">
-          <div className="flex gap-1 justify-center">
-            <BsYoutube className="text-3xl text-red-600" />
-            <span className="text-xl">YouTube</span>
+          <div className="flex gap-1 items-center justify-center">
+            <BsYoutube className="sm:text-3xl text-2xl text-red-600" />
+            <span className="sm:text-xl text-lg">YouTube</span>
           </div>
         </Link>
       </div>
@@ -108,7 +108,7 @@ export default function Sidebar({ filter, setFilter, setCategoryId }:
               className={`pl-6 py-3 hover:bg-neutral-800 ${filter === filterTag ? "bg-neutral-800" : ""}`}
               onClick={() => toggleFilter(filterTag, categoryId)}
             >
-              <h1 className="flex items-center gap-5">
+              <h1 className="flex items-center gap-sm-5 gap-4">
                 {icon}
                 <span className="text-sm tracking-wider">{name}</span>
               </h1>
@@ -123,7 +123,7 @@ export default function Sidebar({ filter, setFilter, setCategoryId }:
               className={`pl-6 py-3 hover:bg-neutral-800 ${filter === filterTag ? "bg-neutral-800" : ""}`}
               onClick={() => { toggleFilter(filterTag, categoryId) }}
             >
-              <h1 className="flex items-center gap-5">
+              <h1 className="flex items-center gap-sm-5 gap-4">
                 {icon}
                 <span className="text-sm tracking-wider">{name}</span>
               </h1>
@@ -131,19 +131,19 @@ export default function Sidebar({ filter, setFilter, setCategoryId }:
           );
         })}
       </ul>
-      <ul className="flex gap-2 flex-wrap text-sm p-4 text-zinc-400">
+      <ul className="flex gap-2 flex-wrap text-sm p-sm-4 p-2 text-zinc-400">
         {textLinks[0].map((name) => {
           return <li key={name}>{name}</li>;
         })}
       </ul>
-      <ul className="flex gap-2 flex-wrap text-sm p-4 text-zinc-400">
+      <ul className="flex gap-2 flex-wrap text-sm p-sm-4 p-2 text-zinc-400">
         {textLinks[1].map((name) => {
           return <li key={name}>{name}</li>;
         })}
       </ul>
-      <span className="px-4 text-sm text-zinc-400">&copy; 2022 Google</span>
+      <span className="px-sm-4 px-2 text-sm text-zinc-400">&copy; 2022 Google</span>
       <br />
-      <p className="px-4 pt-3 text-sm text-zinc-400">
+      <p className="px-sm-4 px-2 pt-3 text-sm text-zinc-400">
         This clone is for educational purpose only.
       </p>
     </div >
