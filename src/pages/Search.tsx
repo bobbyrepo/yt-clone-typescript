024b1fac43cf82a592245032af94f7e501c09402
SC00bidu <you@example.com>
2024-07-18 02:02:14 +0530

    made search responsive

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -33,7 +33,7 @@ function Search({ setSearch }: { setSearch: (query: string) => void }) {
                     loader={<Spinner />}
                     height={680}
                 >
-                    <div className="row row-cols-3 w-[95%] mx-auto mt-6">
+                    <div className="row row-cols-md-3 row-cols-sm-2 row-cols-1 sm:w-[95%] mx-auto mt-6">
                         {searchList.videos.map((item: HomeVideoType) => (
                             <Card data={item} key={item.videoId} />
                         ))}
